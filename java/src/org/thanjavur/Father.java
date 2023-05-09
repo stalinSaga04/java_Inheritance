9e6903e9e27ba6bb9a91644f67441d2657eae6a5
saga <you@example.com>
2023-05-09 01:12:46 -0400

    add programms

---
+++ java/src/org/thanjavur/Father.java
@@ -0,0 +1,18 @@
+package org.thanjavur;
+
+public class Father {
+	private int amount = 10000000;
+	
+	public int get_Cash() {
+		return amount;
+
+	}
+	 void yield(){
+		System.out.println("yield paddy: ");
+	}
+	 protected void share_assets(){
+		System.out.println("share assets: ");
+	}
+	
+	
+}
